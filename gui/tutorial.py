b11ce1437da800470413ad0a95055b026192c23e
Rini Roscoe <rini.roscoe@gmail.com>
2014-07-22 15:49:29 -0500

    Revamp Tutorial
    Bring dealers in to "speak" to the player.

---
+++ gui/tutorial.py
@@ -0,0 +1,444 @@
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.graphics import Color, Rectangle
+from kivy.properties import StringProperty, ObjectProperty, NumericProperty, ListProperty
+from kivy.uix.screenmanager import Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
+
+
+from gui.components import RoundCounter, ScoreDisplay, ToolTipDisplay
+from gui.components import HandDisplay, BoardDisplay
+
+
+class SpeechBubble(Label):
+
+    """Display a speech bubble with word-by-word text."""
+
+    full_text = StringProperty()
+
+    def __init__(self, **kwargs):
+        Label.__init__(self, markup=True, valign="top", halign="left", **kwargs)
+        self.bind(size=self.setter('text_size'))
+
+    def on_full_text(self, *args):
+        Clock.unschedule(self._next_word)
+        self.words_left = self.full_text.split()
+        self.text = self.words_left.pop(0)
+        Clock.schedule_interval(self._next_word, 0.5)
+
+    def _next_word(self, *args):
+        if not self.words_left:
+            Clock.unschedule(self._next_word)
+            return
+        self.text += ' ' + self.words_left.pop(0)
+
+
+class TutorialDealer(Widget):
+
+    """Display a dealer image with a speech bubble."""
+
+    dealer_index = NumericProperty()
+    text = ListProperty()
+    callback = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        Widget.__init__(self, **kwargs)
+        self.layout = BoxLayout()
+        self.dealer = Widget()
+        self.layout.add_widget(self.dealer)
+        self.bubble = SpeechBubble(full_text=self.text.pop(0), size_hint=(.3, 1))
+        self.layout.add_widget(self.bubble)
+        self.add_widget(self.layout)
+        self.bind(size=self.layout.setter('size'))
+        Clock.schedule_once(self._layout)
+
+    def _layout(self, *args):
+        with self.dealer.canvas:
+            Color(1, 1, 1, 1)
+            Rectangle(texture=App.get_running_app().get_dealer_texture(
+                                    self.dealer_index, 0),
+                      size=(min(self.dealer.size[0], self.dealer.size[1]),
+                            min(self.dealer.size[0], self.dealer.size[1])),
+                      pos=self.dealer.pos)
+
+    def on_touch_up(self, touch):
+        self._next_text()
+        return True
+
+    def _next_text(self):
+        """Advance the text in the speech bubble.
+        If no more text, then send a call for the next screen.
+        """
+        if self.bubble.words_left:
+            self.bubble.words_left = []
+            self.bubble.text = self.bubble.full_text
+            return
+        try:
+            self.bubble.full_text = self.text.pop(0)
+        except IndexError:
+            if self.callback is not None:
+                self.callback()
+
+class TutorialScreen(Screen):
+    
+    """Provides common features for Tutorial Screens."""
+
+    next_tutorial = None
+
+    def __init__(self, **kwargs):
+        Screen.__init__(self, **kwargs)
+        Clock.schedule_once(self._make_displays)
+        # ^^ scheduled so that self.manager is set
+
+    def _make_displays(self, *args):
+        """Prepare the display areas."""
+        self.gameboard = BoardDisplay(
+            board=self.manager.main.gameboard.board,
+            size_hint=(4, 1))
+        self.round_counter = RoundCounter(
+            round_number=self.manager.game.round,
+            max_round=self.manager.main.round_counter.max_round,
+            size_hint=(1, .075))
+        self.scoreboard = ScoreDisplay(
+            scoreboard=self.manager.main.scoreboard.scoreboard,
+            size_hint=(1, .4))
+        self.tooltip = ToolTipDisplay(size_hint=(1, .5))
+        self.hand_display = HandDisplay(
+            hand=self.manager.main.hand_display.hand,
+            size_hint=(1, .3))
+        self._layout()
+
+    def _layout(self, *args):
+        """Lay out the display."""
+        self.float = FloatLayout()
+        self.main = main = BoxLayout(orientation="vertical")
+        layout = BoxLayout()
+        layout.add_widget(self.gameboard)
+        self.sidebar = sidebar = BoxLayout(orientation="vertical")
+        sidebar.add_widget(self.round_counter)
+        sidebar.add_widget(self.scoreboard)
+        sidebar.add_widget(self.tooltip)
+        layout.add_widget(sidebar)
+        main.add_widget(layout)
+        main.add_widget(self.hand_display)
+        self.float.add_widget(main)
+        self.add_widget(self.float)
+        Clock.schedule_once(self.draw_tutorial)
+
+    def _fade(self, component):
+        """Make the given component fade into the background."""
+        with component.canvas:
+            Color(0, 0, 0, .75)
+            Rectangle(size=component.size, pos=component.pos)
+
+    def _highlight(self, component):
+        """Fade all but the given component."""
+        for c in (self.gameboard, self.round_counter, self.scoreboard,
+                  self.tooltip, self.hand_display):
+            if c is not component:
+                self._fade(c)
+
+    def draw_tutorial(self, *args):
+        """Draw additional tutorial items."""
+        pass
+
+    def update(self):
+        """Prepare the screen to be shown."""
+        self.gameboard.update()
+        self.hand_display.update()
+
+    def advance(self, *args):
+        """Advance to the next tutorial screen."""
+        if self.next_tutorial is None:
+            self.manager.switcher('home')
+        else:
+            self.manager.switch_to(self.next_tutorial())
+        self.manager.remove_widget(self)
+
+
+    def all_cards_selected(self):
+        return False
+
+    def next_round(self, num, game_over):
+        return False
+
+    def card_touched(self):
+        """Don't allow card touches during text playback."""
+        return True
+
+
+class TutorialActionItemScreen(TutorialScreen):
+
+    """Show the normal game screen, with a text bar across the top."""
+
+    action_item = StringProperty()
+
+    def draw_tutorial(self, *args):
+        self.action_prompt = Label(text='[b]%s[/b]' % self.action_item.upper(),
+                                   markup=True,
+                                   valign="middle", halign="center",
+                                   size_hint=(.75, .2),
+                                   pos_hint={'x':0, 'y':.8})
+        self.float.add_widget(self.action_prompt)
+
+    def on_action_item(self, *args):
+        self.action_prompt.text = self.action_item
+
+    def card_touched(self):
+        """Allow card touches to progress normally."""
+        return False
+
+
+##  Begin Tutorial Screens (in reverse order)  ##
+
+
+class TutorialFinishGame(TutorialActionItemScreen):
+
+    action_item = "Play on!"
+
+
+class TutorialSpecialCardDragged(TutorialScreen):
+
+    text = ["There are many different types of special cards.",
+            "The best cards are unlocked by completing RendezVous Achievements.",
+            "When you unlock a custom deck, it will come with its own unique Special Cards!"]
+
+    next_tutorial = TutorialFinishGame
+
+    def draw_tutorial(self, *args):
+        """Show the dealer text."""
+        self._fade(self.main)
+        self.float.add_widget(TutorialDealer(dealer_index=3,
+                                             text=self.text,
+                                             size_hint=(.8, .8),
+                                             pos=(0, 0),
+                                             callback=self.advance))
+
+
+class TutorialSpecialCardDrag(TutorialActionItemScreen):
+
+    action_item = "Drag the special card below the scoreboard"
+    next_tutorial = TutorialSpecialCardDragged
+    ready = False
+
+    def draw_tutorial(self, *args):
+        with self.tooltip.canvas.before:
+            Color(0, 0, 1, .75)
+            Rectangle(size=self.tooltip.size, pos=self.tooltip.pos)
+        self._fade(self.gameboard)
+        self._fade(self.round_counter)
+        self._fade(self.scoreboard)
+        self.tooltip.bind(card=self._dragged)
+        super(TutorialSpecialCardDrag, self).draw_tutorial(*args)
+        
+    def _dragged(self, *args):
+        self.ready = True
+
+    def card_touched(self):
+        if self.ready:
+            self.advance()
+        return True
+
+
+class TutorialSpecialCard(TutorialScreen):
+
+    text = ["[b]Introducing Special Cards[/b]",
+            "In addition to the five normal suits, you will sometimes get special cards in your hand.",
+            "These can only be played when certain conditions are met.",
+            "Many of them have the power to affect the other cards you play, or even the dealer's cards.",
+            "You drew a special card!",
+            "Drag your new special card onto the tooltip display below the scoreboard to read about what it does and how to use it."]
+
+    next_tutorial = TutorialSpecialCardDrag
+
+    def draw_tutorial(self, *args):
+        """Plant a special card and show dealer text."""
+        self._highlight(self.hand_display)
+        special = self.manager.plant_special()
+        self.text[4] = self.text[4].replace("special", special.name)
+        self.float.add_widget(TutorialDealer(dealer_index=3,
+                                             text=self.text,
+                                             size_hint=(.8, .8),
+                                             pos=(0, 0),
+                                             callback=self.advance))
+
+
+class TutorialPlayAFew(TutorialActionItemScreen):
+
+    action_item = "Play a few rounds!"
+
+    next_tutorial = TutorialSpecialCard
+
+    def draw_tutorial(self, *args):
+        """Leave the tooltip area faded."""
+        self._fade(self.tooltip)
+        self.gameboard.next_round_prompted()
+        super(TutorialPlayAFew, self).draw_tutorial(*args)
+
+    def next_round(self, num, game_over):
+        if num == 11:
+            self.advance()
+            return True
+        return False
+
+
+class TutorialScoreboard(TutorialScreen):
+
+    text = ["Your score in each suit is shown on the left side of the scoreboard.",
+            "The dealer's score is to the right.",
+            "After 20 rounds, the winner is the one leading in the most suits."]
+
+    next_tutorial = TutorialPlayAFew
+    
+    def draw_tutorial(self, *args):
+        """Show the dealer text."""
+        self._highlight(self.scoreboard)
+        self.float.add_widget(TutorialDealer(dealer_index=2,
+                                             reverse=True,
+                                             text=self.text,
+                                             size_hint=(.8, .8),
+                                             pos=(0, 0),
+                                             callback=self.advance))
+
+
+class TutorialScoringReplay(TutorialScreen):
+
+    text = ["A win earns you 10 points in the suit you played, and 10 points in the dealer's suit.",
+            "A loss costs you 10 points in your suit only.",
+            "As the round is scored, the highlighting will help you see who won each match-up."]
+
+    next_tutorial = TutorialScoreboard
+    
+    def draw_tutorial(self, *args):
+        """Replay the scoring sequence behind the dealer text."""
+        self._highlight(self.scoreboard)
+        self.manager._reset_scoring()
+        self.manager._score(then_prompt=False)
+        self.float.add_widget(TutorialDealer(dealer_index=2,
+                                             text=self.text,
+                                             size_hint=(.8, .8),
+                                             pos=(0, 0),
+                                             callback=self.advance))
+
+    def advance(self, *args):
+        """Only advance when the sequence is complete."""
+        if not self.manager._in_progress:
+            super(TutorialScoringReplay, self).advance(*args)
+
+
+class TutorialScoring(TutorialScreen):
+
+    text = ["There are five suits in the deck, and you are scored in each suit."]
+
+    next_tutorial = TutorialScoringReplay
+    
+    def draw_tutorial(self, *args):
+        """Replay the scoring sequence behind the dealer text."""
+        self._highlight(self.scoreboard)
+        self.float.add_widget(TutorialDealer(dealer_index=2,
+                                             text=self.text,
+                                             size_hint=(.8, .8),
+                                             pos=(0, 0),
+                                             callback=self.advance))
+
+
+class TutorialDealerPlay(TutorialScreen):
+
+    text = ["Each card you play is compared against the card directly above it to determine your score.",
+            "Green highlights are the match winners, and red highlights are the losers.",
+            "If the cards tie, then neither one is highlighted."]
+
+    next_tutorial = TutorialScoring
+    
+    def draw_tutorial(self, *args):
+        """Play the scoring sequence behind the dealer text."""
+        self._highlight(self.gameboard)
+        self.manager._score(then_prompt=False)
+        self.float.add_widget(TutorialDealer(dealer_index=1,
+                                             reverse=True,
+                                             text=self.text,
+                                             size_hint=(.8, .8),
+                                             pos=(0, 0),
+                                             callback=self.advance))
+
+    def advance(self, *args):
+        """Only advance when the sequence is complete."""
+        if not self.manager._in_progress:
+            super(TutorialDealerPlay, self).advance(*args)
+
+
+class TutorialDealerTurn(TutorialScreen):
+
+    text = ["[b]My turn![/b]",
+            "I have a hand of 10 cards, just like you.",
+            "Each round, I will pick four cards to play against yours."]
+    
+    next_tutorial = TutorialDealerPlay
+    
+    def draw_tutorial(self, *args):
+        """Show the dealer text while playing in the background."""
+        self._highlight(self.gameboard)
+        self.manager._play_dealer(then_score=False)
+        self.float.add_widget(TutorialDealer(dealer_index=1,
+                                             reverse=True,
+                                             text=self.text,
+                                             size_hint=(.8, .8),
+                                             pos=(0, 0),
+                                             callback=self.advance))
+
+    def advance(self, *args):
+        """Only advance when the play is complete."""
+        if not self.manager._in_progress:
+            super(TutorialDealerTurn, self).advance(*args)
+
+
+class TutorialPickingCards(TutorialActionItemScreen):
+
+    action_item = "Select 4 cards"
+    next_tutorial = TutorialDealerTurn
+
+    def all_cards_selected(self, *args):
+        self.advance()
+        return True  # don't play the dealer's cards yet!
+    
+
+class TutorialPickCards(TutorialScreen):
+
+    text = ["Your hand is displayed below.",
+            "Each round, you will pick 4 cards to play.",
+            "After you play, your hand will be refilled to 10 cards."]
+
+    next_tutorial = TutorialPickingCards
+    
+    def draw_tutorial(self, *args):
+        """Highlight the hand and show the welcome text."""
+        self._highlight(self.hand_display)
+        self.float.add_widget(TutorialDealer(dealer_index=0,
+                                             text=self.text,
+                                             size_hint=(.8, .8),
+                                             pos=(0, 0),
+                                             callback=self.advance))
+
+
+class FirstTutorialScreen(TutorialScreen):
+
+    """Starting point for the tutorial."""
+
+    text = ["[b]Welcome to RendezVous![/b]",
+            "This tutorial will walk you through your first game, introducing key concepts along the way.",
+            "RendezVous is easy to play, but you will find the strategies to be endless!"]
+
+    next_tutorial = TutorialPickCards
+    
+    def draw_tutorial(self, *args):
+        """Cover the game screen items and show the welcome text."""
+        self._fade(self.main)
+        self.float.add_widget(TutorialDealer(dealer_index=0,
+                                             text=self.text,
+                                             size_hint=(.8, .8),
+                                             pos=(0, 0),
+                                             callback=self.advance))
+
--- main.py
+++ main.py
@@ -31,7 +31,6 @@ from gui.components import BLANK, DARKEN
 from gui.components import ConfirmPopup
 from gui.screens.home import HomeScreen
 from gui.screens.game import GameScreen, RoundAchievementScreen
-from gui.screens.tutorial import MainBoardTutorial, SidebarTutorial, TooltipTutorial
 from gui.screens.winner import WinnerScreen
 from gui.settings import SettingSlider, SettingAIDifficulty, SettingButton
 from gui.settings import BackgroundPicker
@@ -41,9 +40,10 @@ from gui.screens.cards import DeckEditScreen
 from gui.screens.statistics import StatisticsScreen
 from gui.screens.achievements import AchievementsScreen
 from gui.screens.powerups import PowerupScreen, CardSelect
+from gui.tutorial import TutorialScreen, FirstTutorialScreen
 
 
-__version__ = '0.7.0'
+__version__ = '0.8.0'
 
 
 class RendezVousWidget(ScreenManager):
@@ -109,11 +109,20 @@ class RendezVousWidget(ScreenManager):
         for hand in self.game.players:
             hand.deck.suits_only()
             hand.flush()
-        self.switch_to(MainBoardTutorial(game=self.game,
-                                         name='tutorial-hand'))
-        self.current_screen.tutorial.title = "Welcome to RendezVous!"
-        self.current_screen.tutorial.text = "This tutorial will walk you through your first game, introducing key concepts along the way. RendezVous is easy to play, but you will find the strategies to be endless!\n\nYour hand is displayed below. Each round, you will pick 4 cards to play, then your hand will be refilled to 10 again. Go ahead and pick your first four cards now.\n\n(Higher values are better!)"
-        self.current_screen.tutorial.footer = "SELECT 4 CARDS BELOW"
+        self.switch_to(FirstTutorialScreen(game=self.game))
+
+    def plant_special(self):
+        """Replace special cards in deck, and place one into the hand."""
+        special = self.app.loaded_deck.specials[1]
+        special = self.app.loaded_deck.get_special(special.name)  # copy
+        self.game.players[PLAYER].cards[8] = special
+        self.game.players[PLAYER].deck.shuffle()
+        self.game.players[DEALER].deck.shuffle()
+        self.current_screen.hand_display.update()
+        for slot in self.current_screen.hand_display.slots:
+            slot.highlight(DARKEN)
+        self.current_screen.hand_display.slots[8].highlight(BLANK)
+        return special
 
     def rebuild_decks(self):
         """Rebuild the deck catalog screen (e.g. after catalog update)."""
@@ -359,14 +368,16 @@ class RendezVousWidget(ScreenManager):
         self._get_dealer_play()
         
     def is_game_screen(self):
-        return self.current == 'main' or self.current.startswith('tutorial')
+        return (self.current == 'main' or self._in_tutorial())
+
+    def _in_tutorial(self):
+        return isinstance(self.current_screen, TutorialScreen)
 
     def card_touched(self, card_display):
         """Handle a touch to a displayed card."""
         if self._in_progress: return
-        if self.current == 'tutorial-tooltip':
-            for slot in self.current_screen.hand_display.slots:
-                slot.highlight(BLANK)
+        if self._in_tutorial():
+            if self.current_screen.card_touched(): return
         elif not self.is_game_screen():
             return
         if self.game.round == 0:  # game over!
@@ -415,6 +426,8 @@ class RendezVousWidget(ScreenManager):
         self.powerup_next_click = None
         # No dragging during scoring (except to tooltip; handled separately)
         if self._in_progress: return
+        elif self._in_tutorial():
+            if self.current_screen.card_touched(): return
         elif not self.is_game_screen():
             return
         
@@ -542,17 +555,9 @@ class RendezVousWidget(ScreenManager):
                     break
             else:
                 self._get_dealer_play()
-        if self.current == 'tutorial-hand':
-            self.switch_to(SidebarTutorial(game=self.game,
-                                           name='tutorial-board'))
-            self.current_screen.tutorial.title = "Now it's the\ndealer's turn!"
-            self.current_screen.tutorial.text = "The dealer has a hand of 10 cards, just like you, and will select four cards to play against yours each round.\n\nEach card you play is compared against the card directly above it to determine your score."
-            self.current_screen.button = Button(text="Continue")
-            self.current_screen.button.bind(on_press=self.score_tutorial)
-        elif self.current == 'tutorial-tooltip':
-            self.switch_to(GameScreen(game=self.game,
-                                      name='tutorial-done'))
-        callback = self._specials if then_score else None
+        if self._in_tutorial():
+            if self.current_screen.all_cards_selected(): return
+        callback = self._specials if then_score else self._mark_ready
         self.current_screen.gameboard.play_dealer(self.dealer_play,
                                                   callback=callback,
                                                   timer=GameSettings.SPEED)
@@ -566,38 +571,36 @@ class RendezVousWidget(ScreenManager):
         """Apply all specials."""
         self.prescore_powerups()
         self.game.board.clear_wait()
-        if self.current == 'tutorial-board':
-            self._in_progress = False
-            return  # until continue is pressed
         self.current_screen.gameboard.apply_specials(self.game,
                     self.current_screen.hand_display, self._score,
                     GameSettings.SPEED)
 
-    def score_tutorial(self, *args):
-        """Continue with scoring from tutorial-board."""
-        if self._in_progress: return
-        self._in_progress = True
-        self.switch_to(SidebarTutorial(game=self.game, name='tutorial-score'))
-        self.current_screen.tutorial.title = "Scoring"
-        self.current_screen.tutorial.text = "There are five suits in the deck, and you are scored in each suit.\n\nA win earns you 10 points in the suit you played, and 10 points in the dealer's suit. A loss costs you 10 points in your suit only.\n\nAs the round is scored, the highlighting will help you see who won each match-up."
-        self._score()
-
-    def _score(self):
+    def _score(self, then_prompt=True):
         """Score the round."""
+        self._in_progress = True
+        callback = self.prompt_for_next_round if then_prompt else self._mark_ready
         self.current_screen.gameboard.score_round(
                 self.current_screen.scoreboard,
-                callback=self.prompt_for_next_round,
+                callback=callback,
                 timer=GameSettings.SPEED)
 
-    def replay_scoring(self):
-        """Replay the scoring sequence at the user's request."""
-        self._in_progress = True
+    def _mark_ready(self):
+        """Let watchers know we are ready to continue."""
+        self._in_progress = False
+
+    def _reset_scoring(self):
+        """Reset for another round of scoring."""
         self.game.score.scores = self._backup_score
         for card in self.game.board:
             card.reset()
         self.current_screen.gameboard.highlight(BLANK)
         self.current_screen.gameboard.update()
         self.current_screen.scoreboard.update()
+
+    def replay_scoring(self):
+        """Replay the scoring sequence at the user's request."""
+        self._in_progress = True
+        self._reset_scoring()
         Clock.schedule_once(lambda dt: self._specials(), GameSettings.SPEED)
 
     def replay_turn(self):
@@ -633,14 +636,13 @@ class RendezVousWidget(ScreenManager):
     def next_round(self):
         """Clear the board for the next round. Return whether to continue."""
         self.cleanup_powerups()
-        if self.current == 'tutorial-score':
-            self.switch_to(TooltipTutorial(game=self.game,
-                                           name='tutorial-continue'))
-            self.current_screen.tutorial.title = "The Scoreboard"
-            self.current_screen.tutorial.text= "Your score in each suit is shown above, to the left. The dealer's score is to the right.\n\nAfter %s rounds, the winner is the one leading in the most suits." % GameSettings.NUM_ROUNDS
-            self.current_screen.tutorial.footer = "Play a few rounds!"
         self.achieved += self.app.record_round(self.game.board)
         game_over = self.game.next_round()
+        self._in_progress = False
+        self._end_of_round = False
+        if self._in_tutorial():
+            if self.current_screen.next_round(self.game.round, game_over):
+                return
         if game_over:
             self.achieved += self.app.record_score(self.game.score)
             self._winner = WinnerScreen(self.game.score, self.achieved,
@@ -656,27 +658,10 @@ class RendezVousWidget(ScreenManager):
             self._play_dealer()
             return False
 
-        if self.current == 'tutorial-continue' and self.game.round >= 10:
-            special = self.app.loaded_deck.specials[0]
-            special = self.app.loaded_deck.get_special(special.name)  # copy
-            self.game.players[PLAYER].cards[8] = special
-            self.app.achievements.achieved.append("Tutorial")
-            self.game.players[PLAYER].deck.shuffle()
-            self.switch_to(MainBoardTutorial(game=self.game,
-                                             name='tutorial-tooltip'))
-            self.current_screen.tutorial.title = "Introducing Special Cards"
-            self.current_screen.tutorial.text = "In addition to the five normal suits, you will sometimes get special cards in your hand. These can only be played when certain conditions are met, but they have the power to affect the other cards you play, or even the dealer's cards.\n\nYou drew a %s card! Drag your new special card onto the tooltip display below the scoreboard to read about what it does and how to use it.\n\nThere are many different types of special cards. The best cards are unlocked by completing RendezVous Achievements - and when you download a custom deck, it will come with its own unique Special Cards!" % special
-            self.current_screen.tutorial.footer = "When you are ready to continue, select your 4 cards for this round."
-            for slot in self.current_screen.hand_display.slots:
-                slot.highlight(DARKEN)
-            self.current_screen.hand_display.slots[8].highlight(BLANK)
-        else:
-            self.current_screen.gameboard.highlight(None)
-            self.current_screen.gameboard.update()
+        self.current_screen.gameboard.highlight(None)
+        self.current_screen.gameboard.update()
         self.current_screen.round_counter.round_number = self.game.round
         self.current_screen.hand_display.update()
-        self._in_progress = False
-        self._end_of_round = False
 
         if self.achieved:
             self.switch_to(RoundAchievementScreen(self.achieved,
@@ -938,7 +923,7 @@ class RendezVousApp(App):
         if self.root:
             self.root.game.load_deck(self.loaded_deck)
             update_deck(self.root.main)
-            if self.root.current[:7] == 'tutorial':
+            if self.root._in_tutorial():
                 update_deck(self.root.current_screen)
 
 
